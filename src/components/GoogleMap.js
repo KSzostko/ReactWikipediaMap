061448db3d0e57a5763d17c13faab04efa7bfc00
Kuba Szostko <kubaszostko@gmail.com>
2021-03-20 20:54:19 +0100

    feat(googlemap): get wikipedia articles for given coords and display then in the console

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
+import { WikiApi } from '../services/api/wiki';
 
 const ReactComponent = () => <div>Hello google map</div>;
 
@@ -11,6 +12,19 @@ const defaultZoom = 15;
 const minZoom = 10;
 
 export default function GoogleMap() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    async function loadArticles() {
+      const newArticles = await WikiApi.getArticles({ coord: defaultCenter });
+      setArticles(newArticles.query.geosearch);
+
+      console.log(newArticles.query.geosearch);
+    }
+
+    loadArticles();
+  }, []);
+
   return (
     <div style={{ height: '91vh', width: '100%' }}>
       <GoogleMapReact
