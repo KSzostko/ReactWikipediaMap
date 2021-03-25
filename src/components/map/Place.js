403e42e64c242847ef28a7d4f844f5cfb372079f
Kuba Szostko <kubaszostko@gmail.com>
2021-03-25 10:44:15 +0100

    feat(place): add hover animation

--- src/components/map/Place.js
+++ src/components/map/Place.js
@@ -8,6 +8,15 @@ const Dot = styled.div`
   height: 30px;
   border-radius: 50%;
   background-color: #fa8c16;
+  opacity: 0.7;
+  transition: all 0.2s ease-in;
+  cursor: pointer;
+  transform: scale(0.95);
+
+  &:hover {
+    opacity: 1;
+    transform: scale(1);
+  }
 `;
 
 export default function Place({ article }) {
