ffc9119a25b44500bb179870667d05d828285394
Kuba Szostko <kubaszostko@gmail.com>
2021-03-25 10:09:57 +0100

    feat(mediator): add hanlder for mapLoaded event

--- src/components/map/eventTypes.js
+++ src/components/map/eventTypes.js
@@ -1,1 +1,2 @@
 export const MAP_DRAGGED = 'mapDragged';
+export const MAP_LOADED = 'mapLoaded';
--- src/components/map/mediator.js
+++ src/components/map/mediator.js
@@ -1,8 +1,9 @@
 import { WikiApi } from '../../services/api/wiki';
-import { useArticles } from './store';
+import { useMapStore } from './store';
 import * as events from './eventTypes';
 
 const listeners = {};
+let map;
 
 export function emit(e, ...args) {
   const listener = listeners[e];
@@ -14,14 +15,21 @@ function addListener(e, listener) {
 }
 
 function useMapMediator() {
-  const [, actions] = useArticles();
+  const [, { addArticles, setGoogleApiLoaded }] = useMapStore();
 
   async function mapDragged(coord) {
     const data = await WikiApi.getArticles({ coord });
 
-    actions.addArticles(data.query.geosearch);
+    addArticles(data.query.geosearch);
   }
 
+  function mapLoaded(mapInstance) {
+    map = mapInstance;
+
+    setGoogleApiLoaded(true);
+  }
+
+  addListener(events.MAP_LOADED, mapLoaded);
   addListener(events.MAP_DRAGGED, mapDragged);
 }
 
