01d99d8a994c5c797b4c66d1b5719c80c3b5a11b
Kuba Szostko <kubaszostko@gmail.com>
2021-03-25 08:49:00 +0100

    feat(Nav): add search input

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Layout } from 'antd';
+import { Layout, Input } from 'antd';
 
 const { Header } = Layout;
+const { Search } = Input;
+
+const StyledHeader = styled(Header)`
+  display: flex;
+  align-items: center;
+`;
 
 const StyledTitle = styled.h1`
   font-size: 24px;
@@ -10,10 +16,15 @@ const StyledTitle = styled.h1`
   font-weight: 600;
 `;
 
+const StyledInput = styled(Search)`
+  margin-left: 20px;
+`;
+
 export default function Nav() {
   return (
-    <Header>
+    <StyledHeader>
       <StyledTitle>WikipediaMap</StyledTitle>
-    </Header>
+      <StyledInput placeholder="Search..." style={{ width: 250 }} />
+    </StyledHeader>
   );
 }
